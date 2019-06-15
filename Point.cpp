ac693895df59e1f0704e2757059c71b6b47fed01
minhtriet <mtriet.chau@gmail.com>
2019-06-15 23:43:09 +0200

    add comparision between Points

--- Point.cpp
+++ Point.cpp
@@ -5,3 +5,6 @@ Point::Point(int x_, int y_): x{x_}, y{y_} {}
 Point operator+(const Point& p0, const Point& p1) {
     return Point(p1.x + p0.x, p1.y + p0.y);
 }
+bool operator==(const Point& p0, const Point& p1) {
+    return (p1.x == p0.x) && (p1.y == p0.y);
+}
--- Point.h
+++ Point.h
@@ -7,4 +7,5 @@ struct Point {
         Point();
         Point(int x_, int y_);
         Point operator+(const Point& p1);
+        bool operator==(const Point& p1);
 };
