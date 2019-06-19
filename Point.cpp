7eb533ff878eb6ef1b6d23701452e4dd6b316c94
minhtriet <mtriet.chau@gmail.com>
2019-06-19 19:35:49 +0200

    add print truct

--- Point.cpp
+++ Point.cpp
@@ -12,4 +12,8 @@ bool operator<(const Point& p0, const Point& p1) {
     return (p1.x < p0.x) || (p1.y < p0.y);
 }
 std::ostream &operator<<(std::ostream& os, Point& const p) {
+    if (p.x == 1 && p.y == 0) return "→ ";
+    if (p.x == -1 && p.y == 0) return "← ";
+    if (p.x == 0 && p.y == 1) return "↑ ";
+    if (p.x == 0 && p.y == -1) return "↓ ";
 }
