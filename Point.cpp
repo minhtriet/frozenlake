27e7162e5057822d49fa897668aded4d36bba0e5
minh <mtriet.chau@gmail.com>
2019-07-10 17:03:50 +0200

    forgot to save

--- Point.cpp
+++ Point.cpp
@@ -2,15 +2,6 @@
 
 Point::Point(): x{0}, y{0} {}
 Point::Point(int x_, int y_): x{x_}, y{y_} {}
-Point operator+(Point p0, const Point& p1) {
-    return Point(p1.x + p0.x, p1.y + p0.y);
-}
-bool operator==(const Point& p0, const Point& p1) {
-    return (p1.x == p0.x) && (p1.y == p0.y);
-}
-bool operator<(const Point& p0, const Point& p1) {
-    return (p1.x < p0.x) || (p1.y < p0.y);
-}
 std::ostream &operator<<(std::ostream& os, const Point& p) {
     if (p.x == 1 && p.y == 0) return os << "→ ";
     if (p.x == -1 && p.y == 0) return os << "← ";
